fb1ad12018ccc1621744647ad9f51dc594d5260d
Marco Antonio R. Gama <71337523+marcogama@users.noreply.github.com>
2023-06-22 22:30:50 -0300

    Update main.java

--- main.java
+++ main.java
@@ -3,7 +3,20 @@ public class main {
 	public static void main(String[] args) {
 		
 		// TODO Auto-generated method stub
-		
+		switch(opcao){
+			case 1:
+				break;
+			case 2:
+				break;
+			case 3:
+				break;
+			case 4:
+				break;
+			case 5:
+				break;
+			default
+				System.out.println("Opcao invalida");
+		}
 
 	}
 
