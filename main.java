b6806e08c27003959b439465eb5a1b84ad2ac5dd
Marco Antonio R. Gama <71337523+marcogama@users.noreply.github.com>
2023-07-26 18:17:11 -0300

    Update main.java

--- main.java
+++ main.java
@@ -1,8 +1,17 @@
+import java.util.*;
+
 public class main {
 
 	public static void main(String[] args) {
 		
 		// TODO Auto-generated method stub
+
+		// Classe Scanner
+		Scanner digite = new Scanner(System.in);
+
+		// Classe incluir_prod
+
+		Incluir_prod newProd = new Incluir_prod(id, cod_prod, 
 		switch(opcao){
 			case 1:
 				break;
