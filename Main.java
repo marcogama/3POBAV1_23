5ad683ed7e731ce2026bff71b59ebb78e97ff445
Marco Antonio R. Gama <71337523+marcogama@users.noreply.github.com>
2023-07-26 18:26:09 -0300

    Update and rename main.java to Main.java

--- Main.java
+++ Main.java
@@ -1,17 +1,21 @@
 import java.util.*;
 
-public class main {
+public class Main {
 
 	public static void main(String[] args) {
 		
 		// TODO Auto-generated method stub
 
+		//Atributos
+		int id, cod_bar;
+		float peso, preco;
+		String sku, nome, descr, fab, categ;
+		
 		// Classe Scanner
 		Scanner digite = new Scanner(System.in);
 
 		// Classe incluir_prod
-
-		Incluir_prod newProd = new Incluir_prod(id, cod_prod, 
+		Incluir_prod newProd = new Incluir_prod(id, cod_bar, peso, preco, sku, nome, descr, fab, categ); 
 		switch(opcao){
 			case 1:
 				break;
